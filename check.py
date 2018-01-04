63aa984a905474b99ffaa14271df4e29db0595e6
yqiu <qiuyj@lansee.net>
2018-01-04 10:40:21 +0800

    init

---
+++ check.py
@@ -0,0 +1,121 @@
+#!/Library/Frameworks/Python.framework/Versions/3.6/bin/python3.6
+
+import os
+import time
+
+mobile, pwd = ('18589245630', '123456')
+
+numberKey = {'1': 8, '2': 9, '3': 10, '4': 11, '5': 12, '6': 13, '7': 14, '8': 15, '9': 16, '0': 7}
+
+init_x, init_y = 760, 1687
+
+# 延迟
+sleep = 5
+
+
+def screenshot():
+    os.system('/usr/local/bin/adb shell screencap -p /sdcard/autojump.png')
+    os.system('/usr/local/bin/adb pull /sdcard/autojump.png /Users/o2o2/github/check_dingding/img')
+
+
+def onClick(x, y):
+    os.system('/usr/local/bin/adb shell input tap %d %d' % (x, y))
+
+
+def inputInfo(info):
+    '''
+    输入信息
+    :param info:
+    :return:
+    '''
+    os.system('/usr/local/bin/adb shell input keyevent %s' % info)
+
+
+def open():
+    '''
+    打开应用
+    :return:
+    '''
+    # 解屏
+    inputInfo(82)
+    time.sleep(sleep)
+    onClick(init_x, init_y)
+    time.sleep(sleep)
+
+
+def login():
+    '''
+    开始登录
+    :return:
+    '''
+    # 点击输入框
+    onClick(700, 700)
+    # 点击清空账号
+    onClick(955, 682)
+    # 输入账号
+    for i in list(mobile):
+        inputInfo(numberKey[i])
+
+    # 跳到输入密码
+    inputInfo(61)
+    # 点击清空密码
+    onClick(955, 908)
+
+    # 输入密码
+    for i in list(pwd):
+        inputInfo(numberKey[i])
+
+    # 关闭键盘
+    inputInfo(4)
+    # 点击登录
+    onClick(530, 1090)
+    time.sleep(sleep)
+
+
+def workspace():
+    '''
+    进入工作区
+    :return:
+    '''
+    onClick(538, 1714)
+    time.sleep(sleep)
+    # 翻页
+    # inputInfo(93)
+    os.system('/usr/local/bin/adb shell input swipe 600 1400 600 100 1000')
+    time.sleep(sleep)
+    # 进入打卡界面
+    onClick(670, 800)
+    time.sleep(sleep)
+
+
+def check():
+    # 上班打卡
+    time.sleep(sleep)
+    onClick(540, 700)
+    # 下班打卡
+    # onClick(540, 1245)
+    time.sleep(sleep)
+    # 退出进程
+    onClick(780, 1870)
+    onClick(544, 1690)
+    inputInfo(4)
+    # 锁屏
+    inputInfo(26)
+
+
+# 截屏
+# screenshot()
+
+# 打开应用
+open()
+
+# 登录
+login()
+
+# 进入工作去
+workspace()
+
+# 打卡
+check()
+
+os.system('echo 打卡成功:`date` >> ~/Desktop/text.txt')
